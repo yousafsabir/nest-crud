66fce2ae02c95c5bbb456cd75c7c94b25eecddc6
yousafsabir <myousafdev@gmail.com>
2023-11-04 23:57:40 +0500

    feat: use db service instead of userService in getUser decorator

--- src/auth/decorator/getUser.decorator.ts
+++ src/auth/decorator/getUser.decorator.ts
@@ -7,23 +7,19 @@ import {
 } from '@nestjs/common';
 
 import { DbService } from 'db.service';
-import { UserService } from 'user/user.service';
 import { User as UserModel } from '@prisma/client';
 
 type UserDataRequirement = 'user' | keyof UserModel;
 
 @Injectable()
 class ParseTokenPipe implements PipeTransform {
-  constructor(
-    private db: DbService,
-    private userService: UserService,
-  ) {}
+  constructor(private db: DbService) {}
 
   async transform(userId: number | undefined, metadata: ArgumentMetadata) {
     if (!userId) return;
     const requirement = metadata.data as UserDataRequirement;
     if (requirement === 'id') return userId;
-    const user = await this.userService.getUser(userId);
+    const user = await this.db.user.findUnique({ where: { id: userId } });
     return requirement === 'user' ? user : user[requirement];
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,6 @@ import { Module, Global } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
-@Global()
 @Module({
   controllers: [UserController],
   providers: [UserService],
