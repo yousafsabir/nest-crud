15c850264b2c0a973085962ad7b00de26e13e6fa
yousafsabir <myousafdev@gmail.com>
2023-11-04 23:45:04 +0500

    feat: guarded bookmark controller

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 
 import { BookmarkService } from 'bookmark/bookmark.service';
 import { CreateBookmarkDTO } from 'bookmark/dto';
+import { JwtGuard } from 'auth/guard';
 
+@UseGuards(JwtGuard)
 @Controller('bookmarks')
 export class BookmarkController {
   constructor(private bookmarkService: BookmarkService) {}
